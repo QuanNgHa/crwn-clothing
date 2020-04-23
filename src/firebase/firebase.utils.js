d500bff4472d99b2972ab3ccdcd5686899142da4
QuanNgHa <quanngha.dev@gmail.com>
2020-04-23 23:33:05 +0800

    Lec 90 - Update Google user via Auth data to FireStore DB

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -6,6 +6,32 @@ import config from './key';
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return; //if userAuth = null => exit this function
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get()
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('Error creating user', error.message);
+
+        }
+    }
+    //we still needs userRef for something
+    return userRef;
+
+}
+
 //Export to access firebase auth anywhere we want
 export const auth = firebase.auth()
 export const firestore = firebase.firestore();
